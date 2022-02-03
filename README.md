96de404aca82093ae2187260acb59287d840f3c4
Ashish Mehra <sdashish.am@gmail.com>
2022-02-03 13:00:27 +0530

    Google custom auto-places component ~ done

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
 # React UI Components
 
+## Google Places
 
+Add below script in head
 
+```
+ <script async=""
+    src="https://maps.googleapis.com/maps/api/js?key={key}&amp;v=3.exp&amp;libraries=places"></script>
+```
+
+### Installation
+
+If using npm,
+
+```bash
+    npm i react-places-autocomplete
+```
+
+or yarn,
+
+```bash
+    yarn add react-places-autocomplete
+```
+
+**_If typescript,_**
+
+npm
+
+```bash
+    npm i -D @types/react-places-autocomplete
+```
+
+yarn
+
+```bash
+    yarn add -D @types/react-places-autocomplete
+```
+
+#### Usage
+
+| Props         | Type                        | Description                                            |
+| :------------ | :-------------------------- | :----------------------------------------------------- |
+| input         | React Element               | **Required**                                           |
+| handleChange  | (data:LocationInfo) => void | **Required** Pass selected address to parent component |
+| value         | String                      | **Required** Render selected value                     |
+| listItemStyle | ListItemStyle               | Custom style for list items                            |
+
+#### Types
+
+```
+type LocationInfo = {
+  lat: number;
+  lng: number;
+  address: string;
+  postalCode: string | null;
+};
+
+type ListItemStyle = {
+  hover?: React.CSSProperties;
+  style: React.CSSProperties;
+};
+
+```
---
+++ src/components/GooglePlaces/LocationInput.tsx
@@ -0,0 +1,102 @@
+import React, { HTMLInputTypeAttribute, useEffect, useState } from "react";
+import PlacesAutocomplete, { getLatLng, geocodeByPlaceId } from "react-places-autocomplete";
+
+export type LocationInfo = {
+  lat: number;
+  lng: number;
+  address: string;
+  postalCode: string | null;
+};
+
+type ListItemStyle = {
+  hover?: React.CSSProperties;
+  style: React.CSSProperties;
+};
+
+interface LocationInput {
+  input: React.ElementType;
+  handleChange: (data: LocationInfo) => void;
+  value: string;
+  listItemStyle: ListItemStyle;
+}
+
+const LocationInput: React.FC<LocationInput> = ({ value, handleChange, input, listItemStyle }) => {
+  const [address, setAddress] = useState("");
+
+  const handleSelect = async (address: string, placeId: string) => {
+    setAddress(address);
+
+    const response = await geocodeByPlaceId(placeId);
+
+    if (response?.length) {
+      const result = response[0];
+      let postalCode = null;
+
+      const { lat, lng } = await getLatLng(result);
+
+      result?.address_components.map((component) => {
+        if (component.types.includes("postal_code")) {
+          postalCode = component.short_name || component.long_name;
+        }
+      });
+
+      handleChange({
+        lat,
+        lng,
+        address,
+        postalCode,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (value) setAddress(value);
+  }, [value]);
+
+  const Input = input as "input";
+
+  return (
+    <PlacesAutocomplete
+      value={address}
+      onChange={(address) => setAddress(address)}
+      onSelect={handleSelect}
+      searchOptions={{ componentRestrictions: { country: "AU" } }}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <>
+          <h2>Location</h2>
+          <Input
+            id="location"
+            name="location"
+            {...getInputProps({
+              classname: "location-search-input",
+            })}
+          />
+          <div className="autocomplete-dropdown-container">
+            {loading && <span>Loading....</span>}
+            {suggestions.map((suggestion) => {
+              let style = { ...listItemStyle.style };
+
+              if (listItemStyle.hover && suggestion.active) {
+                style = { ...style, ...listItemStyle.hover };
+              }
+
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className: "suggestion-item",
+                    style,
+                  })}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </PlacesAutocomplete>
+  );
+};
+
+export default LocationInput;
