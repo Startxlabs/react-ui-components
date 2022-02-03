854325c5fe8d6f0a2fc3975c4927f18242af5f6d
Ashish Mehra <sdashish.am@gmail.com>
2022-02-03 16:38:59 +0530

    Stripe custom form added with validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { StripeForm } from "./components/StripeForm";
+
+const simulateFetch = async () => {
+  return "seti_";
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <StripeForm
+        fetchSetupIntent={simulateFetch}
+        handleException={(message) => console.error(message)}
+        handleSuccess={(response) => {
+          console.log({ response });
+        }}
+      />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -12,7 +24,7 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/StripeForm/CheckoutForm.tsx
@@ -0,0 +1,184 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { loadStripe, SetupIntent, SetupIntentResult, StripeElementStyle } from "@stripe/stripe-js";
+import {
+  CardCvcElement,
+  CardExpiryElement,
+  CardNumberElement,
+  Elements,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe("publishableKey");
+
+type CardInputValidation = {
+  valid: boolean;
+  message: string;
+};
+
+const initialValidationValues = (message): CardInputValidation => ({ valid: false, message });
+
+const errorMessages = {
+  cardRequired: "Please enter card number.",
+  expRequired: "Please enter expiry date.",
+  cvcRequired: "Please enter cvc number.",
+};
+
+interface CheckoutForm {
+  style?: StripeElementStyle | undefined;
+  fetchSetupIntent: () => Promise<string>;
+  handleException: (message: string) => void;
+  handleSuccess: (data: SetupIntentResult) => void;
+}
+
+const StripeForm: React.FC<CheckoutForm> = ({ style, ...props }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [showErrors, setShowErrors] = useState(false);
+
+  const cardNumValidation = useRef<CardInputValidation>(initialValidationValues(errorMessages.cardRequired));
+  const cardExpValidation = useRef<CardInputValidation>(initialValidationValues(errorMessages.expRequired));
+  const cardCvcValidation = useRef<CardInputValidation>(initialValidationValues(errorMessages.cvcRequired));
+
+  const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+
+      if (cardNumValidation.current.valid && cardExpValidation.current.valid && cardCvcValidation.current.valid) {
+        /**
+         * If card is valid fetch setup intent
+         */
+        let setupIntentToken = await props.fetchSetupIntent();
+
+        if (setupIntentToken) {
+          const result = await stripe.confirmCardSetup(setupIntentToken, {
+            payment_method: {
+              card: elements.getElement(CardNumberElement),
+            },
+          });
+
+          if (result?.error) {
+            const message = result?.error?.message;
+            props.handleException(message);
+          }
+
+          props.handleSuccess(result);
+        }
+      } else {
+        setShowErrors(true);
+      }
+    } catch (error) {
+      console.log({ error });
+      props.handleException("Check logs for stripe errror.");
+    }
+  };
+
+  const cardNumberHandler = (event) => {
+    const isValid = !event.error && !event.empty;
+
+    let message = null;
+
+    if (!isValid) {
+      message = event.empty ? "Please enter card number." : event.error?.message;
+    }
+
+    cardNumValidation.current = {
+      ...cardNumValidation.current,
+      valid: isValid,
+      message,
+    };
+  };
+
+  const cardExpiryHandler = (event) => {
+    const isValid = !event.error && !event.empty;
+
+    let message = null;
+
+    if (!isValid) {
+      message = event.empty ? "Please enter expiry date." : event.error?.message;
+    }
+
+    cardExpValidation.current = {
+      ...cardExpValidation.current,
+      valid: isValid,
+      message,
+    };
+  };
+
+  const cardCvcHandler = (event) => {
+    const isValid = !event.error && !event.empty;
+
+    let message = null;
+
+    if (!isValid) {
+      message = event.empty ? "Please enter cvc number." : event.error?.message;
+    }
+
+    cardCvcValidation.current = {
+      ...cardExpValidation.current,
+      valid: isValid,
+      message,
+    };
+  };
+
+  useEffect(() => {
+    if (elements) {
+      const cardNumberElement = elements.getElement(CardNumberElement),
+        cardExpiryElement = elements.getElement(CardExpiryElement),
+        cardCvcElement = elements.getElement(CardCvcElement);
+
+      if (cardNumberElement && cardExpiryElement && cardCvcElement) {
+        cardNumberElement.on("change", cardNumberHandler);
+        cardExpiryElement.on("change", cardExpiryHandler);
+        cardCvcElement.on("change", cardCvcHandler);
+      }
+    }
+  }, [elements]);
+
+  return (
+    <form noValidate onSubmit={onFormSubmit}>
+      <div>
+        <label>Card Number</label>
+        <CardNumberElement
+          options={{
+            style,
+            placeholder: "",
+          }}
+        />
+        {showErrors && !cardNumValidation.current.valid && <p>{cardNumValidation.current.message}</p>}
+      </div>
+      <div>
+        <label>Expiry Date</label>
+        <CardExpiryElement
+          options={{
+            style,
+            placeholder: "",
+          }}
+        />
+        {showErrors && !cardExpValidation.current.valid && <p>{cardExpValidation.current.message}</p>}
+      </div>
+      <div>
+        <label>CVV</label>
+        <CardCvcElement
+          options={{
+            style,
+            placeholder: "",
+          }}
+        />
+        {showErrors && !cardCvcValidation.current.valid && <p>{cardCvcValidation.current.message}</p>}
+      </div>
+      <button type="submit" disabled={!stripe || !elements}>
+        Add Card
+      </button>
+    </form>
+  );
+};
+
+const CheckoutForm = (props: CheckoutForm) => (
+  <Elements stripe={stripePromise}>
+    <StripeForm {...props} />
+  </Elements>
+);
+
+export default CheckoutForm;
